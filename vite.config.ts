5436d525d8bd59f9a2710029bb6d1d1258bd4834
Yu Nozato <mojahige@gmail.com>
2021-07-03 15:50:20 +0900

    Create Vite Project (#14)
    * update gitignore
    
    Node.js プロジェクトのテンプレートを追加
    
    * create Vite project
    
    - change development directory
    - change dist directory
    - add @types/noded
    
    * upgrade typescript
    
    * fix publicDir setting
    
    お外に行ってしまってた
    
    * update TypeScript setting
    
    - add `useUnknownInCatchVariables`
    - add `exactOptionalPropertyTypes`

---
+++ vite.config.ts
@@ -0,0 +1,13 @@
+
+import path from 'path'
+import { defineConfig } from 'vite'
+import reactRefresh from '@vitejs/plugin-react-refresh'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  root: './dev',
+  build: {
+    outDir: path.join(__dirname, 'dist'),
+  },
+  plugins: [reactRefresh()]
+})
