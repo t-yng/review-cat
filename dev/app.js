9da81cf4a02a923617f789025cbe09b26969b26b
mojahige <mojahige@gmail.com>
2021-07-03 21:12:03 +0900

    不要な変数削除

--- dev/app.js
+++ dev/app.js
@@ -1,30 +1,30 @@
-const path = require('path')
-const { app, BrowserWindow } = require('electron')
+const path = require('path');
+const { app, BrowserWindow } = require('electron');
 
-const isDevelopment = process?.env?.NODE_ENV === 'development'
-
-let mainWindow = null
+const isDevelopment = process?.env?.NODE_ENV === 'development';
 
 function createWindow() {
-  mainWindow = new BrowserWindow({ width: 530, height: 496 })
+  const mainWindow = new BrowserWindow({ width: 530, height: 496 });
+
   mainWindow.loadURL(
     isDevelopment
       ? 'http://localhost:3000/'
       : `file://${path.resolve(__dirname, './index.html')}`
-  )
-  mainWindow.on('closed', () => (mainWindow = null))
+  );
 }
 
-app.on('ready', createWindow)
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
 
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
-    app.quit()
+    app.quit();
   }
-})
-
-app.on('activate', () => {
-  if (mainWindow === null) {
-    createWindow()
-  }
-})
+});
