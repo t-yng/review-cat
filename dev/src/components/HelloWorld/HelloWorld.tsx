d5bc94a7415115b1b36c012c80691d9d95ce6395
t-yng <t.yng.greenmonkey@gmail.com>
2021-07-07 22:08:28 +0900

    ユニットテストが動いてなかったので修正してサンプルコードを追加した

---
+++ dev/src/components/HelloWorld/HelloWorld.tsx
@@ -0,0 +1,3 @@
+export const HelloWord = () => {
+  return <div>Hello World</div>;
+};
--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,8 @@
 module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
   testPathIgnorePatterns: ['/node_modules/', '<rootDir>/lib/'],
-  transform: {
-    '^.+\\.tsx?$': '<rootDir>/node_modules/ts-jest/preprocessor.js',
-  },
   testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json'],
-  snapshotSerializers: ['enzyme-to-json/serializer'],
-  setupFiles: ['./test/jestsetup.js'],
+  setupFilesAfterEnv: ['./test/setup.ts'],
 };
