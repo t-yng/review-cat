47b628698bd4c1a78f266c8696973e6e029eb1b2
t-yng <t.yng.greenmonkey@gmail.com>
2021-07-13 20:41:58 +0900

    prelaod.js を TypeScript 実装に置き換え

--- electron/preload.ts
+++ electron/preload.ts
@@ -1,11 +1,11 @@
-const { contextBridge, ipcRenderer } = require('electron');
+import { contextBridge, ipcRenderer } from 'electron';
 
 contextBridge.exposeInMainWorld('ipc', {
   loginWithGithub: async () => {
     const code = await ipcRenderer.invoke('loginWithGithub');
     return code;
   },
-  getAccessToken: async (code) => {
+  getAccessToken: async (code: string) => {
     const token = await ipcRenderer.invoke('getAccessToken', code);
     return token;
   },
