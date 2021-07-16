1a33ff2f6e3992493d85d7bbddafc9447d59cc15
mojahige <mojahige@gmail.com>
2021-07-16 23:03:10 +0900

    cosme

--- renderer/src/main.tsx
+++ renderer/src/main.tsx
@@ -4,7 +4,9 @@ import './index.css';
 import App from './App';
 
 const rootElement = document.getElementById('root');
+
 if (!rootElement) throw new Error('Failed to find the root element');
+
 const root = ReactDOM.createRoot(rootElement);
 
 root.render(
