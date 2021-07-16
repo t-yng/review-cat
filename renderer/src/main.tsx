5ed9ec7b6e5335dc378d7bac06b714eb451b74ea
mojahige <mojahige@gmail.com>
2021-07-16 23:02:12 +0900

    React18 の API を使用するように変更
    createRoot を使用するようになった。

--- renderer/src/main.tsx
+++ renderer/src/main.tsx
@@ -3,9 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-ReactDOM.render(
+const rootElement = document.getElementById('root');
+if (!rootElement) throw new Error('Failed to find the root element');
+const root = ReactDOM.createRoot(rootElement);
+
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
