d124b2a84c8d40a0b9b624e1a27df49b56febea1
mojahige <mojahige@gmail.com>
2021-07-17 00:42:58 +0900

    不要なスタイルを削除

--- renderer/src/App.tsx
+++ renderer/src/App.tsx
@@ -1,6 +1,5 @@
 import React, { Suspense } from 'react';
 import { Provider, useAtom } from 'jotai';
-import './App.css';
 import { LoginPage } from './pages/Login';
 import { tokenAtomWithPersistence } from './jotai/auth';
 import { PullRequestListPage } from './pages/PullRequestList';
