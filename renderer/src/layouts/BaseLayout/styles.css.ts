13b3e0fbacc8d9a1db4d269dba59cedd9d870311
mojahige <mojahige@gmail.com>
2021-07-17 23:21:40 +0900

    ナビ表示要素のスタイル調整
    position を sticky にして、スクロールが発生する状態でも左側に固定表示されるようにした。

--- renderer/src/layouts/BaseLayout/styles.css.ts
+++ renderer/src/layouts/BaseLayout/styles.css.ts
@@ -1,10 +1,17 @@
 import { style } from '@vanilla-extract/css';
 
-export const baseLayoutStyle = style({
+export const rootStyle = style({
   display: 'grid',
   gridTemplateColumns: '64px 1fr',
   gridTemplateRows: '1fr',
   width: '100%',
-  height: '100vh',
+  minHeight: '100vh',
   backgroundColor: '#fff',
+});
+
+export const navContainerStyle = style({
+  position: 'sticky',
+  top: '0',
+  left: '0',
+  height: '100vh',
 });
