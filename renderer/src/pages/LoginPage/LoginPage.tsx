7791632b12917c708c670162888676d7dc30a964
mojahige <mojahige@gmail.com>
2021-07-17 12:50:05 +0900

    LoginPage コンポーネント整理
    - pages の中にディレクトリ追加
    - バレル化
    - import 調整

--- renderer/src/pages/LoginPage/LoginPage.tsx
+++ renderer/src/pages/LoginPage/LoginPage.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import { BaseLayout } from '../layouts/BaseLayout';
+import { BaseLayout } from '../../layouts/BaseLayout';
 import { useAtom } from 'jotai';
-import { signInAtom } from '../jotai/auth';
+import { signInAtom } from '../../jotai/auth';
 
 export const LoginPage = () => {
   const [, signIn] = useAtom(signInAtom);
