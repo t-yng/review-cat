7be5232594dd18779bfc34777e5ab25e3020ff0e
t-yng <t-yng@users.noreply.github.com>
2021-08-01 12:37:38 +0900

    Reactのバージョンをv18:alpha => v17に変更した (#52)

--- renderer/src/main.tsx
+++ renderer/src/main.tsx
@@ -5,12 +5,9 @@ import App from './App';
 
 const rootElement = document.getElementById('root');
 
-if (!rootElement) throw new Error('Failed to find the root element');
-
-const root = ReactDOM.createRoot(rootElement);
-
-root.render(
+ReactDOM.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
+  rootElement
 );
