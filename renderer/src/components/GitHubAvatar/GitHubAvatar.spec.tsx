4b7e1f55d5f7d2ac7b6f5e825a808d85f0f3dfab
mojahige <mojahige@gmail.com>
2021-08-05 01:40:02 +0900

    it に揃える

--- renderer/src/components/GitHubAvatar/GitHubAvatar.spec.tsx
+++ renderer/src/components/GitHubAvatar/GitHubAvatar.spec.tsx
@@ -3,7 +3,7 @@ import { render, screen } from '@testing-library/react';
 import { GitHubAvatar } from './GitHubAvatar';
 
 describe('GitHubUserAvatar', () => {
-  test('alt props が無い場合は 空の alt 属性が指定される', () => {
+  it('alt props が無い場合は 空の alt 属性が指定される', () => {
     render(<GitHubAvatar src="#" />);
 
     expect(screen.getByAltText('')).toBeInTheDocument();
