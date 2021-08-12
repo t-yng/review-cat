e0dd55f56925669efe25183f6787257e2c290e3c
t-yng <t.yng.greenmonkey@gmail.com>
2021-08-12 21:04:02 +0900

    リポジトリ選択へのルーティングを実装

--- renderer/src/components/routes/AppRoute/AppRoute.tsx
+++ renderer/src/components/routes/AppRoute/AppRoute.tsx
@@ -1,17 +1,35 @@
 import React from 'react';
-import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
-import { LoginPage, PullRequestListPage } from '../../../pages';
+import {
+  Route,
+  BrowserRouter as Router,
+  Switch,
+  Redirect,
+} from 'react-router-dom';
+import { useSettings } from '../../../hooks/useSettings';
+import {
+  LoginPage,
+  PullRequestListPage,
+  SelectRepositoriesPage,
+} from '../../../pages';
 import { PrivateRoute } from '../PrivateRoute';
 
 export const AppRoute = () => {
+  const { settings } = useSettings();
   return (
     <Router>
       <Switch>
         <Route path="/login">
           <LoginPage />
         </Route>
+        <PrivateRoute path="/select-repositories">
+          <SelectRepositoriesPage />
+        </PrivateRoute>
         <PrivateRoute path="/">
-          <PullRequestListPage />
+          {settings.subscribedPRList.length === 0 ? (
+            <Redirect to="/select-repositories" />
+          ) : (
+            <PullRequestListPage />
+          )}
         </PrivateRoute>
       </Switch>
     </Router>
