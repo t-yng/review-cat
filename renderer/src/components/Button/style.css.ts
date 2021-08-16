e99d255f90efc832b5490216260955159c9f7f03
t-yng <t.yng.greenmonkey@gmail.com>
2021-08-16 22:53:26 +0900

    ボタンのline-heithを変更

--- renderer/src/components/Button/style.css.ts
+++ renderer/src/components/Button/style.css.ts
@@ -3,11 +3,11 @@ import { themeVars } from '../../theme.css';
 import { space } from '../../themeStyleHelper';
 
 export const rootStyle = style({
-  padding: `${space(1)} ${space(2)}`,
+  padding: `${space(0.5)} ${space(2)}`,
   backgroundColor: themeVars.color.accent,
   borderRadius: '4px',
   color: themeVars.color.white,
   fontSize: '1.6rem',
   fontWeight: 700,
-  lineHeight: 1,
+  lineHeight: 1.5,
 });
