209bf2715d02a33f710d4ff0bd62879a57f354f7
mojahige <mojahige@gmail.com>
2021-08-27 23:46:14 +0900

    React.memo の書き方を修正

--- renderer/src/components/GitHubAvatar/GitHubAvatar.tsx
+++ renderer/src/components/GitHubAvatar/GitHubAvatar.tsx
@@ -1,13 +1,11 @@
-import React from 'react';
+import React, { memo } from 'react';
 import { rootStyle } from './styles.css';
 
 interface Props extends React.ImgHTMLAttributes<HTMLImageElement> {
   src: string;
 }
 
-export const GitHubAvatar: React.FC<Props> = React.memo(
-  ({ alt = '', ...others }) => {
-    // TODO: GitHub のアバター画像、パラメータ(`s={size}`)でサイズ調整できるのでうまいこと利用したい。
-    return <img className={rootStyle} alt={alt} {...others} />;
-  }
-);
+export const GitHubAvatar: React.FC<Props> = memo(({ alt = '', ...others }) => {
+  // TODO: GitHub のアバター画像、パラメータ(`s={size}`)でサイズ調整できるのでうまいこと利用したい。
+  return <img className={rootStyle} alt={alt} {...others} />;
+});
