3a1c6628253c8fe9134bfa54d4810d870b748d29
t-yng <t.yng.greenmonkey@gmail.com>
2021-08-28 11:15:37 +0900

    監視するリポジトリ一覧を設定から読み込み

--- renderer/src/pages/PullRequestListPage/PullRequestListPage.tsx
+++ renderer/src/pages/PullRequestListPage/PullRequestListPage.tsx
@@ -4,6 +4,7 @@ import { loadQuery, useRelayEnvironment } from 'react-relay';
 import { PullRequestListContainer } from '../../containers/PullRequestListContainer/PullRequestListContainer';
 import { SearchPullRequestsQuery } from '../../graphql/queries';
 import { buildSearchPullRequestsQuery } from '../../graphql/queries/builder';
+import { useSettings } from '../../hooks';
 import { loginUserAtom } from '../../jotai';
 import { BaseLayout } from '../../layouts/BaseLayout';
 
@@ -12,6 +13,7 @@ export const PullRequestListPage: FC = () => {
   // ここでユーザー情報の更新をすると親コンポーネントで Suspense でラップする必要がありナビゲーション含めてフォールバックされる
   const environment = useRelayEnvironment();
   const [user] = useAtom(loginUserAtom);
+  const { settings } = useSettings();
 
   // REFACTOR: user のnullチェックが不要な形に変更できない？
   return (
@@ -20,12 +22,9 @@ export const PullRequestListPage: FC = () => {
         <PullRequestListContainer
           preloadedQuery={loadQuery(environment, SearchPullRequestsQuery, {
             login_user_name: user?.name ?? '',
-            // TODO: 購読するリポジトリの一覧を設定から取得
-            search_query: buildSearchPullRequestsQuery([
-              't-yng/blog',
-              'higeOhige/review-cat',
-              'higeOhige/review-cat-test-repo',
-            ]),
+            search_query: buildSearchPullRequestsQuery(
+              settings.subscribedRepositories
+            ),
           })}
         />
       </Suspense>
