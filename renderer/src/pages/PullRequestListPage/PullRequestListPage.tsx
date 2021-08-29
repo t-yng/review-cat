ffe276fb79fa6456e06d23e01199916020785bc9
t-yng <t-yng@users.noreply.github.com>
2021-08-29 11:20:44 +0900

    プルリクエストの一覧を定期的にポーリングする (#75)

--- renderer/src/pages/PullRequestListPage/PullRequestListPage.tsx
+++ renderer/src/pages/PullRequestListPage/PullRequestListPage.tsx
@@ -1,5 +1,5 @@
+import React, { FC, Suspense, useCallback, useEffect, useState } from 'react';
 import { useAtom } from 'jotai';
-import React, { FC, Suspense } from 'react';
 import { loadQuery, useRelayEnvironment } from 'react-relay';
 import { PullRequestListContainer } from '../../containers/PullRequestListContainer/PullRequestListContainer';
 import { SearchPullRequestsQuery } from '../../graphql/queries';
@@ -7,6 +7,11 @@ import { buildSearchPullRequestsQuery } from '../../graphql/queries/builder';
 import { useSettings } from '../../hooks';
 import { loginUserAtom } from '../../jotai';
 import { BaseLayout } from '../../layouts/BaseLayout';
+import { SearchPullRequestsQuery as SearchPullRequestsQueryType } from '../../graphql/queries/__generated__/SearchPullRequestsQuery.graphql';
+
+// 頻度が多いと Github GraphQL API のレート制限に影響するので、投げるクエリのスコアを計算して調整してください。
+// @see: https://docs.github.com/ja/graphql/overview/resource-limitations
+const FETCH_PULL_REQUESTS_INTERVAL = 60 * 1000; // ms
 
 export const PullRequestListPage: FC = () => {
   // TODO: ユーザー情報の取得周りは AppStateProvider, useAppState を作成してそちらに寄せる
@@ -15,18 +20,39 @@ export const PullRequestListPage: FC = () => {
   const [user] = useAtom(loginUserAtom);
   const { settings } = useSettings();
 
-  // REFACTOR: user のnullチェックが不要な形に変更できない？
+  const loadPullRequests = useCallback(() => {
+    return loadQuery<SearchPullRequestsQueryType>(
+      environment,
+      SearchPullRequestsQuery,
+      {
+        // REFACTOR: user のnullチェックが不要な形に変更できない？
+        login_user_name: user?.name ?? '',
+        search_query: buildSearchPullRequestsQuery(
+          settings.subscribedRepositories
+        ),
+      },
+      { fetchPolicy: 'store-and-network' }
+    );
+  }, [environment, user?.name, settings]);
+
+  const [preloadedQuery, setPreloadedQuery] = useState(() => {
+    return loadPullRequests();
+  });
+
+  useEffect(() => {
+    const timerId = setInterval(() => {
+      setPreloadedQuery(loadPullRequests());
+    }, FETCH_PULL_REQUESTS_INTERVAL);
+
+    return () => {
+      clearInterval(timerId);
+    };
+  }, [setPreloadedQuery, loadPullRequests]);
+
   return (
     <BaseLayout>
       <Suspense fallback="Loading...">
-        <PullRequestListContainer
-          preloadedQuery={loadQuery(environment, SearchPullRequestsQuery, {
-            login_user_name: user?.name ?? '',
-            search_query: buildSearchPullRequestsQuery(
-              settings.subscribedRepositories
-            ),
-          })}
-        />
+        <PullRequestListContainer preloadedQuery={preloadedQuery} />
       </Suspense>
     </BaseLayout>
   );
