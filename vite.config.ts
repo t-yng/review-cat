a169dbc0948a25c5388df7416fdf455748d42bd4
t-yng <t.yng.greenmonkey@gmail.com>
2021-09-05 18:17:23 +0900

    update: relayのパッケージ削除と不要な設定の削除

--- vite.config.ts
+++ vite.config.ts
@@ -2,19 +2,13 @@ import path from 'path';
 import { defineConfig } from 'vite';
 import reactRefresh from '@vitejs/plugin-react-refresh';
 import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin';
-import relay from 'vite-plugin-relay';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  define: {
-    // Viteのdevサーバー実行時にrelay@11.0.2で global is not defined が発生するので、その対策
-    // @see: https://github.com/vitejs/vite/discussions/3859
-    global: 'window',
-  },
   root: './renderer',
   base: process.env.NODE_ENV === 'production' ? '' : '/',
   build: {
     outDir: path.join(__dirname, 'dist'),
   },
-  plugins: [reactRefresh(), vanillaExtractPlugin(), relay],
+  plugins: [reactRefresh(), vanillaExtractPlugin()],
 });
