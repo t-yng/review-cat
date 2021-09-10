6dff5c41507f4b9fc1f77dcf6c0e59ff1570843b
t-yng <t.yng.greenmonkey@gmail.com>
2021-09-10 22:21:43 +0900

    chore: publish build のために設定を変更

--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,16 @@ import reactRefresh from '@vitejs/plugin-react-refresh';
 import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin';
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   root: './renderer',
-  base: process.env.NODE_ENV === 'production' ? '' : '/',
+  base: mode === 'production' ? '' : '/',
+  // @see: https://github.com/apollographql/apollo-client/issues/8661
+  define: {
+    __DEV__: false,
+  },
   build: {
     outDir: path.join(__dirname, 'dist'),
+    emptyOutDir: true,
   },
   plugins: [reactRefresh(), vanillaExtractPlugin()],
-});
+}));
