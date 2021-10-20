3ecba352622a9cf9e3b20573bdbf229fa955a1f5
t-yng <t.yng.greenmonkey@gmail.com>
2021-10-20 13:25:26 +0900

    アプリロゴのalt属性を修正

--- renderer/src/components/AppIcon/AppIcon.tsx
+++ renderer/src/components/AppIcon/AppIcon.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
 
 export const AppIcon: React.FC = () => {
-  return <img src="/assets/logo.svg" alt="アプリのロゴ" />;
+  return <img src="/assets/logo.svg" alt="review-cat のロゴ" />;
 };
