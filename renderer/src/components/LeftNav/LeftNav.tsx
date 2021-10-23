c910be6ea045105332238d9d702314c4db4b0c35
t-yng <t.yng.greenmonkey@gmail.com>
2021-10-23 14:37:08 +0900

    プルリク一覧へ移動するリンクをナビゲーションバーに追加

--- renderer/src/components/LeftNav/LeftNav.tsx
+++ renderer/src/components/LeftNav/LeftNav.tsx
@@ -4,14 +4,30 @@ import { useHistory } from 'react-router';
 import { loginUserAtom, signOutAtom } from '../../jotai';
 import { GitHubAvatar } from '../GitHubAvatar';
 import { UserMenu } from '../UserMenu';
-import { navStyle, userIconStyle } from './styles.css';
+import {
+  iconStyle,
+  navStyle,
+  pullRequestIconLink,
+  statusCountBadge,
+  userIconStyle,
+} from './styles.css';
+import { GitPullRequestIcon } from '@primer/octicons-react';
+import { Link } from 'react-router-dom';
+import { usePullRequests } from '../../hooks';
+import { PullRequest } from '../../models';
 
 export const LeftNav: React.FC = () => {
+  usePullRequests;
   const history = useHistory();
   const avatarRef = useRef<HTMLElement>(null);
   const [user] = useAtom(loginUserAtom);
   const [, signOut] = useAtom(signOutAtom);
   const [visibleUserMenu, setVisibleUserMenu] = useState(false);
+  const { pullRequests, loading } = usePullRequests();
+
+  const requestedReviewPullRequests = (pullRequests: PullRequest[]) => {
+    return pullRequests.filter((pr) => pr.status === 'requestedReview');
+  };
 
   useEffect(() => {
     const handleClickBody = (event: MouseEvent) => {
@@ -46,10 +62,24 @@ export const LeftNav: React.FC = () => {
           <GitHubAvatar
             src={user?.avatarUrl}
             onClick={handleClickUserIcon}
-            className={userIconStyle}
+            className={`${iconStyle} ${userIconStyle}`}
           />
         )}
       </h1>
+      <div>
+        <Link to="/" className={pullRequestIconLink}>
+          <GitPullRequestIcon
+            size={24}
+            className={iconStyle}
+            aria-label="プルリクエスト一覧へ移動"
+          />
+          {!loading && (
+            <span role="status" className={statusCountBadge}>
+              {requestedReviewPullRequests(pullRequests).length}
+            </span>
+          )}
+        </Link>
+      </div>
       {user != null && visibleUserMenu && (
         <UserMenu user={user} onClickSignOut={handleSignOut} />
       )}
--- renderer/src/components/LeftNav/styles.css.ts
+++ renderer/src/components/LeftNav/styles.css.ts
@@ -3,13 +3,44 @@ import { themeVars } from '../../theme.css';
 import { space } from '../../themeStyleHelper';
 
 export const navStyle = style({
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
   height: '100%',
   padding: `${space(4)} ${space(2)}`,
   backgroundColor: themeVars.color.gray900,
 });
 
-export const userIconStyle = style({
+export const pullRequestIconLink = style({
+  display: 'flex',
+  alignItems: 'center',
+  gap: space(0.5),
+  ':hover': {
+    opacity: 0.8,
+  },
+});
+
+export const iconStyle = style({
+  color: themeVars.color.gray100,
   ':hover': {
     cursor: 'pointer',
   },
+});
+
+export const userIconStyle = style({
+  marginBottom: space(4),
+});
+
+export const statusCountBadge = style({
+  borderRadius: '50%',
+  background: themeVars.color.gray100,
+  color: themeVars.color.gray700,
+  width: space(2),
+  height: space(2),
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+  fontSize: '1.2rem',
+  fontWeight: 'bold',
+  lineHeight: 1.0,
 });
