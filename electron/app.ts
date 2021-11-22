5dae67f713925da7c6fb42a06b7747067190443c
t-yng <t.yng.greenmonkey@gmail.com>
2021-11-22 17:19:19 +0900

    アプリをタスクトレイ化した

--- electron/app.ts
+++ electron/app.ts
@@ -1,61 +1,56 @@
 import path from 'path';
-import { app, BrowserWindow, ipcMain, shell } from 'electron';
+import { app, ipcMain } from 'electron';
+import { menubar } from 'menubar';
 import { auth } from './src/lib';
 import { oAuthOptions } from './src/constants/auth';
 
 const isDevelopment = process?.env?.NODE_ENV === 'development';
 
-function createWindow() {
-  if (process.platform === 'darwin') {
-    const appIcon = path.join(__dirname, 'assets', 'images', 'app-icon.png');
-    app.dock.setIcon(appIcon);
+const trayIcon = path.join(__dirname, 'assets', 'images', 'tray-icon.png');
+const indexUrl = isDevelopment
+  ? 'http://localhost:3000/'
+  : `file://${path.resolve(__dirname, './index.html')}`;
+
+const browserWindowOpts = {
+  width: 500,
+  height: 400,
+  minWidth: 500,
+  minHeight: 400,
+  resizable: false,
+  webPreferences: {
+    enableRemoteModule: true,
+    overlayScrollbars: true,
+    nodeIntegration: false,
+    contextIsolation: true,
+    preload: path.join(__dirname, 'preload.js'),
+  },
+};
+
+const menubarApp = menubar({
+  icon: trayIcon,
+  index: indexUrl,
+  browserWindow: browserWindowOpts,
+  preloadWindow: true,
+});
+
+const delayedHideAppIcon = () => {
+  if (app.dock && app.dock.hide) {
+    // Setting a timeout because the showDockIcon is not currently working
+    // See more at https://github.com/maxogden/menubar/issues/306
+    setTimeout(() => {
+      app.dock.hide();
+    }, 1000);
   }
+};
 
-  const mainWindow = new BrowserWindow({
-    width: 530,
-    height: 496,
-    webPreferences: {
-      nodeIntegration: false,
-      contextIsolation: true,
-      preload: path.join(__dirname, 'preload.js'),
-    },
+menubarApp.on('ready', () => {
+  delayedHideAppIcon();
+
+  ipcMain.handle('loginWithGithub', async () => {
+    return auth.loginWithGithub(oAuthOptions);
   });
 
-  mainWindow.webContents.setWindowOpenHandler((details) => {
-    shell.openExternal(details.url);
-
-    return {
-      action: 'deny',
-    };
+  ipcMain.handle('getAccessToken', async (event, code: string) => {
+    return auth.getGithubOAuthToken(oAuthOptions, code);
   });
-
-  const indexUrl = isDevelopment
-    ? 'http://localhost:3000/'
-    : `file://${path.resolve(__dirname, './index.html')}`;
-
-  mainWindow.loadURL(indexUrl);
-}
-
-app.whenReady().then(() => {
-  createWindow();
-
-  app.on('activate', () => {
-    if (BrowserWindow.getAllWindows().length === 0) {
-      createWindow();
-    }
-  });
-});
-
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit();
-  }
-});
-
-ipcMain.handle('loginWithGithub', async () => {
-  return auth.loginWithGithub(oAuthOptions);
-});
-
-ipcMain.handle('getAccessToken', async (event, code: string) => {
-  return auth.getGithubOAuthToken(oAuthOptions, code);
 });
