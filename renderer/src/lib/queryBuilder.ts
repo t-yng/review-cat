07ec228b87b27282443340bec8d273853387ddee
t-yng <t-yng@users.noreply.github.com>
2021-11-25 08:54:30 +0900

    設定リポジトリの一覧が未設定の時にプルリクのバッジ件数が1件以上になるバグの修正 (#120)

--- renderer/src/lib/queryBuilder.ts
+++ renderer/src/lib/queryBuilder.ts
@@ -1,5 +1,10 @@
-export const buildSearchPullRequestsQuery = (repositories: string[]) => `
-type:pr state:open involves:@me -author:@me ${repositories
-  .map((repo) => `repo:${repo}`)
-  .join(' ')}
-`;
+export const buildSearchPullRequestsQuery = (repositories: string[]) => {
+  if (repositories.length === 0) {
+    return '';
+  } else {
+    return `type:pr state:open involves:@me -author:@me ${repositories
+      .map((repo) => `repo:${repo}`)
+      .join(' ')}
+    `;
+  }
+};
