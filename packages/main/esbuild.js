ca2de7d38469a17dc23f7803b0a52a10f79727c8
t-yng <t.yng.greenmonkey@gmail.com>
2023-01-06 00:03:46 +0900

    esbuildを修正

--- packages/main/esbuild.js
+++ packages/main/esbuild.js
@@ -2,7 +2,6 @@ require('esbuild').build({
   bundle: true,
   // NOTE: electronをバンドルすると実行パスチェックでエラーが発生するので、バンドルしない
   packages: 'external',
-  // external: ['electron', 'web'],
   entryPoints: ['src/app.ts', 'src/preload.ts'],
   outdir: 'dist',
   define: {
@@ -10,7 +9,7 @@ require('esbuild').build({
       ? `"${process.env.NODE_ENV}"`
       : '"development"',
     'process.env.GITHUB_APP_CLIENT_ID': `"${process.env.GITHUB_APP_CLIENT_ID}"`,
-    'process.env.GITHUB_APP_CLIENT_SECRET': `"${process.env.GITHUB_APP_CLIENT_ID}"`,
+    'process.env.GITHUB_APP_CLIENT_SECRET': `"${process.env.GITHUB_APP_CLIENT_SECRET}"`,
   },
   platform: 'node',
 });
