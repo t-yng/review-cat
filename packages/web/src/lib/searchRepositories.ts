4f52b7170bfaef79ec656ac1c88b7816ff999ba2
t-yng <t.yng.greenmonkey@gmail.com>
2023-01-05 17:23:09 +0900

    eslintの設定ファイルを管理するパッケージを追加

--- packages/web/src/lib/searchRepositories.ts
+++
@@ -1,4 +0,0 @@
-export const searchRepositories = (account: string, keyword: string) => {
-  // TODO: 何かメッチャいい感じにリポジトリを検索する！
-  return Promise.resolve(['higeOhige/review-cat', 'higeOhige/test1']);
-};
